86960811b1941fa4ea28f8f7183ade8768e5db0a
imomarksasa <41761073+imomarksasa@users.noreply.github.com>
2018-09-22 19:12:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,47 @@ client.on('message', message => {
   	}
 });
 
+client.on('ready',async () => {
+  console.log(client.user.username);
+  try {
+    const config = {
+      name: "DISCO", // اسم الرتبة
+      guildid: "359674601392832512", // اي دي السيرفر
+      sec: 1 // عدد الثواني
+    };
+    let guild = client.guilds.get(config.guildid);
+    let role = guild.roles.find(role => role.name === config.name);
+    let sec = config.sec * 1000;
+    if(!guild) return console.warn("Unkown guild.");
+    if(!role) return console.warn("Unkown role");
+    if(role.position >= guild.members.get(client.user.id).highestRole.position) return console.warn("Bot highest role must be above rainbow role");
+    setInterval(() => {
+      role.edit({ 
+      color: "RANDOM"
+    });
+    }, sec);
+  } catch(e) {
+    console.error(e);
+  }
+});
+
+client.on('ready', function(){
+    var ms = 10000 ;
+    var setGame = [`Bot ON ${client.guilds.size} Servers`,`In Server ${client.users.size} Users`];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/KiNg66S`);
+    }, ms);
+
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
